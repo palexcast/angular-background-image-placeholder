f8af7ab3c633489d5ed528a3f1bfad5d72a47f89
Alexander Castillo <alexander_castillo@bitbucket.org>
2016-10-09 10:57:01 +0200

    REMAKE: Directive remade to include initial background-image
    There were too many bugs when not including the initial initalisation of the background-image. It also opened for more freedom for transitions and better performance

--- src/directives/ac-background-image-placeholder.js
+++ src/directives/ac-background-image-placeholder.js
@@ -1,52 +1,64 @@
 (function () {
-  /* global angular, Image */
+  /* global angular, document, Image, requestAnimationFrame */
 
   'use strict';
   angular
-    .module('ac-background-image-placeholder.directives')
-    .directive('acBackgroundImagePlaceholder', acBackgroundImagePlaceholder);
+    .module('sf-admin.directives')
+    .directive('acBackgroundImage', acBackgroundImage);
 
-	 /**
-     * Temporary sets the element's `ac-background-image-placeholder` attribute's value as the element's CSS background-image
-	 * while waiting for element's CSS background-image to load.
-     * 
-     * Usage: 
-     *    <element style="background-image: url(./cats.jpg);"
-	 *     ac-background-image-placeholder="data:image/png;base64,..."></element>
-     * 
-     * Temporary Result:
-     *    <element style="background-image: url(data:image/png;base64,...);"
-	 *     ac-background-image-placeholder="data:image/png;base64,..."></element>
-	 * 
-     * End Result:
-     *    <element style="background-image: url(./cats.jpg);"
-	 *     ac-background-image-placeholder="data:image/png;base64,..."></element>
-     */
-  function acBackgroundImagePlaceholder() {
+  function acBackgroundImage() {
     return {
       restrict: 'A',
-      scope: {
-        acBackgroundImagePlaceholder: '@'
-      },
       link: link
     };
 
     function link(scope, element, attrs) {
-      scope.$watch(function () {
-        return element[0].style.backgroundImage;
-      }, function () {
-        var backgroundUrl = element[0].style.backgroundImage.slice(4, -1).replace(/"/g, '');
-        element[0].style.backgroundImage = 'url("' + attrs.acBackgroundImagePlaceholder + '")';
+      var backgroundDiv = createSubDiv();
+      var placeholderDiv = createSubDiv();
+      backgroundDiv.className = 'ac-bg-img__full-size';
+      placeholderDiv.className = 'ac-bg-img__placeholder';
+      placeholderDiv.style.opacity = 1;
+      placeholderDiv.style.zIndex = 1;
+      element[0].style.position = 'relative';
+      element[0].appendChild(backgroundDiv);
+      element[0].appendChild(placeholderDiv);
 
-        var image = new Image();
-        image.onload = function () {
-          element.css({
-            background: 'url(' + this.src + ')'
-          });
-        };
-
-        image.src = backgroundUrl;
+      attrs.$observe('placeholder', function () {
+        placeholderDiv.style.backgroundImage = 'url(' + attrs.placeholder + ')';
       });
+      attrs.$observe('backgroundImage', backgroundChange);
+
+      function createSubDiv() {
+        var div = document.createElement('div');
+        div.style.position = 'absolute';
+        div.style.left = 0;
+        div.style.top = 0;
+        div.style.bottom = 0;
+        div.style.right = 0;
+        div.style.height = '100%';
+        return div;
+      }
+
+      function backgroundChange() {
+        placeholderDiv.style.opacity = 1;
+        var image = new Image();
+        image.addEventListener('load', function () {
+          backgroundDiv.style.backgroundImage = 'url(' + this.src + ')';
+          fadeOut(placeholderDiv);
+        });
+        image.src = attrs.backgroundImage;
+      }
+
+      function fadeOut(el) {
+        el.style.opacity = 1;
+        (function fade() {
+          if ((el.style.opacity -= 0.1) <= 0) {
+            el.style.display = 'none';
+          } else {
+            requestAnimationFrame(fade);
+          }
+        })();
+      }
     }
   }
 })();
